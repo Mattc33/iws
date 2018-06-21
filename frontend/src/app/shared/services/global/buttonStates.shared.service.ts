d0796d341278edde6651eb5f1e2a89b4a7f59311
Matt Chan <m.chan@obietechologies.com>
2018-06-21 10:54:19 -0400

    move to root

--- frontend/src/app/shared/services/global/buttonStates.shared.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class ToggleButtonStateService {
-
-    toggleButtonStates = (gridSelectionStatus: number): boolean => 
-        gridSelectionStatus > 0 ? false : true
-
-}
